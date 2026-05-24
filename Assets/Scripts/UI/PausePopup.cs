1b0974ce179fcd8e33eb774f01a98fff7b11f9f0
Nara <dlskfk72@gmail.com>
2026-05-25 01:36:25 +0900

    [PausePopup] PausePopup.cs 생성 및 로비 이동 버튼 기능 구현

---
+++ Assets/Scripts/UI/PausePopup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PausePopup : UIBase
+{
+    [SerializeField] private GameUIButton Btn_ContinueGame;
+    [SerializeField] private GameUIButton Btn_GoToLobbyMain;
+
+    private void OnEnable()
+    {
+        Btn_GoToLobbyMain.BindOnClickButtonEvent(OnClick_GoToLobbyMain);
+    }
+
+    private void OnClick_ContinueGame()
+    {
+        
+    }
+
+    private void OnClick_GoToLobbyMain()
+    {
+        UIManager.Instance.OpenLoadingUI();
+        UIManager.Instance.CloseGameViewUI();
+        UIManager.Instance.ClosePausePopup();
+        UIManager.Instance.OpenLobbyMainUI();
+    }
+}
