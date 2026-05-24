127d3f6b973fda4d659f3c8342cdd3813ac31e63
Nara <dlskfk72@gmail.com>
2026-05-25 00:15:02 +0900

    [GameViewUI] GameViewUI.cs 생성 및 플레이어 이름, 레벨 UI 연동

---
+++ Assets/Scripts/UI/GameViewUI.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class GameViewUI : MonoBehaviour
+{
+    [Header("플레이어 정보")]
+    [SerializeField] private TMP_Text Text_PlayerName;
+    [SerializeField] private TMP_Text Text_PlayerLevel;
+
+    public void Start()
+    {
+        PlayerInfo();
+    }
+
+    private void PlayerInfo()
+    {
+        var player = GameDataManager.Instance.GetCharacterData("character_selly_01");
+        if (player == null) return;
+
+        Text_PlayerName.text = player.Name;
+
+        var playerLevel = GameManager.Instance.GetPlayerLevel();
+        Text_PlayerLevel.text = $"Lv.{playerLevel}";
+    }
+}
