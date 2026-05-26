8020af8c05ca43de2907279957b1247aca90d428
Nara <dlskfk72@gmail.com>
2026-05-26 17:39:06 +0900

    [2D Monster] 추후 몬스터 스킬 확장을 고려해 몬스터 스킬 로직 구현

--- Assets/Scripts/2D/Monster2D.cs
+++ Assets/Scripts/2D/Monster2D.cs
@@ -1,7 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Monster2D : MonoBehaviour
 {
+    //[Header("몬스터 프리팹에서 미리 세팅할 데이터")]
+    //public float SkillCoolTime;
+    //public GameObject Prefab_MonsterSkillObject;
+
     [Header("데이터를 확인할 수 있도록 임시로 열어줌")]
     public int _monsterInstanceId;
     private string _monsterDataId;
@@ -14,6 +19,13 @@ public class Monster2D : MonoBehaviour
     private MonsterData _monsterData;
     public int _baseHp;
     public int _baseAtk;
+    public bool _isAlive = true;
+    //private bool _lookRight = true;
+
+    private void OnDisable()
+    {
+        _isAlive = false;
+    }
 
     private void Start()
     {
@@ -41,12 +53,24 @@ public class Monster2D : MonoBehaviour
 
         _monsterInstanceId = instanceId;
         _monsterDataId = monsterDataId;
+
+        //StartCoroutine(CheckAndUseSkill());
     }
 
-    public string GetFieldObjectDataId()
+    public string GetMonsterDataId()
     {
         return _monsterDataId;
     }
+
+    //private int GetFinalNormalAtkDamage(int baseAtk, float normalAtkMultiple)
+    //{
+    //    return GetFinalSkillDamage(baseAtk, normalAtkMultiple);
+    //}
+
+    //private int GetFinalSkillDamage(int baseAtk, float skillMultiple)
+    //{
+    //    return (int)(baseAtk * skillMultiple);
+    //}
 
     void RandomPickDirection()
     {
@@ -70,4 +94,35 @@ public class Monster2D : MonoBehaviour
         transform.position += _moveDirection * 5.0f * Time.deltaTime;
     }
 
+    // 코루틴이 등장한단는건 -> 유니태스크로 호환이 가능하다
+    // 일정 시간마다 스킬을 사용할 예정
+    // 스타트 코루틴은 이 몬스터가 생성된 시점에서 돌아도 됨!
+    //IEnumerator CheckAndUseSkill()
+    //{
+    //    while(_isAlive)
+    //    {
+    //        yield return new WaitForSeconds(2.0f);
+
+    //        if(_isAlive == false)
+    //        {
+    //            break;
+    //        }
+
+    //        UseSkill();
+    //    }
+    //}
+
+    private void UseSkill()
+    {
+        //var gObj = Instantiate(Prefab_MonsterSkillObject, GameObjectManager.Instance.transform);
+        //if (gObj == null) return;
+
+        //var skillProjectileComponent = gObj.GetComponent<SkillProjectile>();
+        //if(skillProjectileComponent == null) return;
+
+        // // TODO : 추후 함수로 빠져야함
+        //float skillMultiple = _monsterData.SkillAtkMultipleList.Count > 0 ? _monsterData.SkillAtkMultipleList[0] : 0;
+        //int finalSkillDamage = GetFinalSkillDamage(_baseAtk, skillMultiple)
+        //skillProjectileComponent.InitSkillObject(_lookRight, this.transform.position, finalSkillDamage);
+    }
 }
