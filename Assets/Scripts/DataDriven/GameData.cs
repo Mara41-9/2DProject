77daf4faa030c3d1a00ddfcd3fda0e13f00a7c13
Nara <dlskfk72@gmail.com>
2026-05-27 20:56:34 +0900

    [GameData] 아이템 'ItemType' 데이터 필드 GameData에 반영

--- Assets/Scripts/DataDriven/GameData.cs
+++ Assets/Scripts/DataDriven/GameData.cs
@@ -52,6 +52,7 @@ public class ItemData : GameDataBase
 {
     public string Name;
     public string Description;
+    public string ItemType;
     public string Grade;
     public int MaxStackCount;
     public int SellingPrice;
