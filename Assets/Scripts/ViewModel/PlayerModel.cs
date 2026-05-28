870e790fff251f7cd29bf666fe83f1b68115f625
Nara <dlskfk72@gmail.com>
2026-05-28 18:28:19 +0900

    [Rule] 아이템과 같이 저장이 가능한 데이터는 저장할 때 InstanceId 대신 UniqueId를 쓰자. 몬스터와 같이 저장이 안 되도 되는 데이터는 InstanceId를 쓴다

--- Assets/Scripts/ViewModel/PlayerModel.cs
+++ Assets/Scripts/ViewModel/PlayerModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-
 // 플레이어 전체 저장 데이터를 담는 클래스
 // JsonUtility로 직렬화하려면, Mono를 상속받지 않도록 주의하자!
 // [Serializable] : 이 클래스를 JSON으로 저장하거나 불러올 수 있게 해주는 표시
