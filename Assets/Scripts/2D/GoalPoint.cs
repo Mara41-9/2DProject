d0803928afa8ed9c0a701db9ff53531682e5999f
Nara <dlskfk72@gmail.com>
2026-06-02 18:39:45 +0900

    [Fix - 2D Player] Toto Collider Capsule로 변경

---
+++ Assets/Scripts/2D/GoalPoint.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GoalPoint : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") == true)
+        {
+            UIManager.Instance.OpenSuccessPopup();
+        }
+    }
+   
+}
