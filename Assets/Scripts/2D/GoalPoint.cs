7daeccef8131b486a8cb184980f0d6ed55258fef
Nara <dlskfk72@gmail.com>
2026-06-02 18:59:37 +0900

    [GoalPoint] GoalPoint 오브젝트 만들고 충돌한 객체가 플레이어라면 게임성공 팝업 뜨도록 로직 구현

--- Assets/Scripts/2D/GoalPoint.cs
+++ Assets/Scripts/2D/GoalPoint.cs
@@ -2,6 +2,11 @@
 
 public class GoalPoint : MonoBehaviour
 {
+    private void Start()
+    {
+        Debug.Log("GoalPoint Start");
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player") == true)
