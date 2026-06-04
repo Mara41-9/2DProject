81b454b8eab5e50af171c3e245cdea23c76546f7
Nara <dlskfk72@gmail.com>
2026-06-05 00:33:18 +0900

    [GameData] 스킬 'MaxUseCount' 데이터 필드 GameData에 반영

--- Assets/Scripts/DataDriven/GameData.cs
+++ Assets/Scripts/DataDriven/GameData.cs
@@ -28,6 +28,7 @@ public class SkillData : GameDataBase
 {
     public string Name;
     public string Description;
+    public int MaxUseCount;
     public string IconPath;
     public string PrefabPath;
 }
