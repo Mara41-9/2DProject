25cee26f42de1c89c4c2bd9df380a29fa241bd26
Nara <dlskfk72@gmail.com>
2026-06-04 10:00:15 +0900

    [SuccessPopup] 얻은 아이템, 몬스터 슬롯 프리팹 생성 및 SuccessPopupSlotUI.cs 기본 로직 구현

---
+++ Assets/Scripts/UI/SuccessPopupSlotUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SuccessPopupSlotUI : MonoBehaviour
+{
+    [Header("슬롯 정보")]
+    [SerializeField] private Image Image_Bg;
+    [SerializeField] private Image Image_ObtainedContents;
+    [SerializeField] private Text Text_StackCount;
+}
