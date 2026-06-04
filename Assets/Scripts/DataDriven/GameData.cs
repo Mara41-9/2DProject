f07585b0dba4ba3df2f3ee64a10bf96c1e632601
Nara <dlskfk72@gmail.com>
2026-06-04 22:03:42 +0900

    [GameData] 스킬 'PrefabPath' 데이터 필드 GameData에 반영

--- Assets/Scripts/DataDriven/GameData.cs
+++ Assets/Scripts/DataDriven/GameData.cs
@@ -29,6 +29,7 @@ public class SkillData : GameDataBase
     public string Name;
     public string Description;
     public string IconPath;
+    public string PrefabPath;
 }
 
 [System.Serializable]
