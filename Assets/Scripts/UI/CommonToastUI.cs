31fff18443ccba7954b5b18536d559d42ffc9c1b
Nara <dlskfk72@gmail.com>
2026-06-05 14:26:44 +0900

    [CommonToastUI] 상황에 따라 토스트 UI 위치를 변경할 수 있도록 기능 추가

--- Assets/Scripts/UI/CommonToastUI.cs
+++ Assets/Scripts/UI/CommonToastUI.cs
@@ -8,6 +8,13 @@ public class CommonToastUI : UIBase
     [SerializeField] private Image Image_Bg;
     [SerializeField] private Image Image_Frame;
     [SerializeField] private TMP_Text Text_Message;
+    [SerializeField] private RectTransform ToastUI_Position;
+
+    public void SetPosition(Vector2 pos)
+    {
+        // anchoredPosition : 앵커(Anchor)를 기준으로 한 위치
+        ToastUI_Position.anchoredPosition = pos;
+    }
 
     public void SetMessage(string message)
     {
