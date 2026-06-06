afe7e15ed46e7c3011118d2893c3350d0c41bbd6
Nara <dlskfk72@gmail.com>
2026-06-06 13:46:07 +0900

    [MonsterInfoPopup] MonsterInfoPopup.cs 기본 로직 구성

---
+++ Assets/Scripts/UI/MonsterInfoPopup.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class MonsterInfoPopup : MonoBehaviour
+{
+    [SerializeField] private TMP_Text Text_MonsterName;
+    [SerializeField] private TMP_Text Text_MonsterDesc;
+    [SerializeField] private TMP_Text Text_MonsterAtk;
+
+    private void OnEnable()
+    {
+        SetPopupUI();
+    }
+
+    private void SetPopupUI()
+    {
+        
+    }
+}
